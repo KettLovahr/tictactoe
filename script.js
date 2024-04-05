673e524c119f68e831d4c9003fed7c467dc3375a
KettLovahr <catusfelony@gmail.com>
2024-04-05 20:46:44 -0300

    ehhehehee
    terminei

--- script.js
+++ script.js
@@ -3,11 +3,65 @@ var board = document.getElementById("board");
 var log = document.getElementById("log");
 
 var x_turn = true;
+var game_arr = [null, null, null, null, null, null, null, null, null]
+var game_history = []
+var game_end = false;
 
 for (let i = 0; i < board.children.length; i++) {
     let el = board.children[i]
     el.onclick = function() {
-        el.classList.add(x_turn ? "cross" : "naught");
-        x_turn = !x_turn;
+        if (!el.classList.contains("cross") && !el.classList.contains("naught") && !game_end) {
+            el.classList.add(x_turn ? "cross" : "naught");
+            game_arr[i] = x_turn ? 'x' : 'o';
+            game_history.push(i);
+
+            if (game_history.length > 5) {
+                clear_tile(game_history.shift());
+            }
+            
+    
+            let new_entry = document.createElement("div");
+            new_entry.innerHTML = `Foi colocado um ${x_turn ? "X" : "O"} no quadrado ${i}`
+            log.appendChild(new_entry);
+    
+            x_turn = !x_turn;
+            who_won()
+        }
     }
+}
+
+function who_won() {
+    // I could probably do something smarter, but I want to be done quick.
+    // Let's check all the different win scenarios
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for (let w = 0; w < wins.length; w++) {
+        if (game_arr[wins[w][0]] != null && game_arr[wins[w][0]] == game_arr[wins[w][1]] && game_arr[wins[w][1]] == game_arr[wins[w][2]]) {
+            let new_entry = document.createElement("div");
+            new_entry.innerHTML = `${game_arr[wins[w][0]]} ganhou`
+            log.appendChild(new_entry);
+            board.children[wins[w][0]].style.backgroundColor = "white";
+            board.children[wins[w][1]].style.backgroundColor = "white";
+            board.children[wins[w][2]].style.backgroundColor = "white";
+
+            game_end = true;
+            return game_arr[wins[w][0]]; //return
+        }
+    }
+    return null;
+}
+
+function clear_tile(c) {
+    game_arr[c] = null
+    board.children[c].classList.remove("naught");
+    board.children[c].classList.remove("cross");
 }
